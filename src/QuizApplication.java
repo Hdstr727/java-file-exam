b5314beb97be2f0f59bfa9d9974dbf98fdb60f08
Hdstr727 <helldestroerr05@gmail.com>
2023-06-01 13:15:35 +0300

    Projekta uzsākšana, izveidots sākuma logs

---
+++ src/QuizApplication.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class QuizApplication extends JFrame{
+    public QuizApplication() {
+        setTitle("Quiz Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
